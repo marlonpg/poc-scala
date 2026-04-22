e4fcba2dd7de756f3de533c5979306ec60030008
Marlon Gamba <marlonpgamba@gmail.com>
2026-04-21 22:23:27 -0300

    Adding example of SealedTraits.

---
+++ src/basics/SealedTraits.scala
@@ -0,0 +1,17 @@
+//> using scala 3.8.3
+
+// A sealed trait means all implementations must be declared in this file.
+// This helps the compiler check pattern matches more safely.
+sealed trait PaymentMethod
+case object Cash extends PaymentMethod
+case class CreditCard(number: String) extends PaymentMethod
+case class Pix(key: String) extends PaymentMethod
+
+@main
+def sealedTraits(): Unit =
+    println("Sealed Traits in Scala")
+
+    def paymentLabel(method: PaymentMethod): String = method match
+        case Cash => "Paying with cash"
+        case CreditCard(number) => s"Paying with credit card ending in ${number.takeRight(4)}"
+        case Pix(key) => s"Paying with PIX key: $key"
